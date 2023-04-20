d64e768dbfcb349ebac14ca927c2ef867812b9b1
javilak <javilak@javilak.com>
2023-04-21 03:36:17 +0800

    upload go.mod,update map.go

--- map/map.go
+++ map/map.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Printf(1)
-
+	var s = "how do you do"
+	var wordc = make(map[string]int, 10)
+	var words = strings.Split(s, " ")
+	for _, word := range words {
+		v, ok := wordc[word]
+		if ok {
+			wordc[word] = v + 1
+		} else {
+			wordc[word] = 1
+		}
+	}
+	for k, v := range wordc {
+		fmt.Println(k, v)
+	}
 }
